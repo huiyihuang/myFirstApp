72e54feff89e5a8348609e81e845db709acd1aa7
Huiyi Huang <huanghy@umich.edu>
2018-09-08 19:42:31 -0400

    upload

---
+++ src/pages/about/about.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-about',
+  templateUrl: 'about.html'
+})
+export class AboutPage {
+  now: Date;
+  author: string;
+  constructor(public navCtrl: NavController) {
+    this.now = new Date();
+    this.author = 'Huiyi Huang'; 
+  }
+
+}
